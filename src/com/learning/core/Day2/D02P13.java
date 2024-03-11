7ca9963cdb00671742bb0f565f097bd35bb7c7cd
24Sanjay <113135126+24Sanjay@users.noreply.github.com>
2024-03-11 17:01:15 +0530

    Add files via upload

---
+++ src/com/learning/core/Day2/D02P13.java
@@ -0,0 +1,15 @@
+package com.learning.core.day2;
+import  java.util.Scanner;
+public class D02P13 {
+	 public static void main(String[] args) {
+	        Scanner sc= new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int k=1;
+	        for(int i=1;i<=a;i++) {
+	        	k=k*i;
+	        	
+	        	
+	        	}
+	        System.out.println(k);
+	        }
+}
